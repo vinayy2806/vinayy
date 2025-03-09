4c1f00337900e5bb3c35cde3d75f1483a91bae5a
vinay <vinay28623@gmail.com>
2025-03-09 11:58:35 +0530

    commit the git code

--- terrform/main.tf
+++ terrform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = ""
-    secret_key = ""
+    access_key = "4U"
+    secret_key = "4y0c6a4YKwR"
 
 }
 
@@ -9,18 +9,17 @@ resource "aws_instance" "admin"{
     ami = "ami-0ff20c640f06d85cf"
     instance_type = "t2.medium"
     security_groups = [ "default" ]
-    key_name = ""
+    key_name = "project"
     root_block_device {
       volume_size = 20
       volume_type = "gp2"
       delete_on_termination = true
     }
     tags = {
-      Name = "testing"
+      Name = "admin"
     }   
 }    
 output "public_ip" {
     value = aws_instance.admin.public_ip
-
 
 }
